75250f948f0b9aefbb9ca82854eb6400c3a4672d
Hunter  Quant <hunterdquant@gmail.com>
2016-02-26 14:02:11 -0500

    Implemented Quicksort.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,8 +3,27 @@
 using namespace std;
 
 template <class T>
-vector<T> quicksort(vector<T> & vec) {
+int partition(vector<T> & vec, int p, int r) {
+ 	T pivot = vec[r];
+	int i = p-1;
+	for (int j = p; j <= r; j++) {
+		if (vec[j] <= pivot) {
+			i++;
+			T temp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = temp;
+		}
+	}
+	return i;
+}
 
+template <class T>
+void quicksort(vector<T> & vec, int p, int r) {
+	if (p < r) {
+		int q = partition(vec, p, r);
+		quicksort(vec, p, q-1);
+		quicksort(vec, q+1, r);
+	}
 }
 
 template <class T>
@@ -13,6 +32,9 @@ vector<T> insertionsort(vector<T> & vec) {
 }
 
 int main() {
-	cout << "Hello World\n";
+	vector<int> v = {3,12,9,5,20, 123,345, 13 ,678 ,134, 869, 1, 34 ,78, 666, 420, 69};
+	quicksort(v, 0, v.size()-1);
+	for(int i : v)
+		cout << i << '\n';
 	return 0;
 }
