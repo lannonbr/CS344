4bca9dc02b7412b3b08e142a9e149eb0a6787a48
Hunter  Quant <hunterdquant@gmail.com>
2016-03-05 13:29:45 -0500

    Added default Integer constructor as well as add 1 to count for each comparison

--- Project1/integer.h
+++ Project1/integer.h
@@ -1,12 +1,15 @@
 class Integer {
 	public:
+		Integer(): _value(0), _count(0) {};
 		Integer(int x): _value(x), _count(0) {};
 		int get_value() { return _value; };
 		int get_count() { return _count; };
 		bool operator<(const Integer& other) {
+			_count++;
 			return _value < other._value;
 		}
 		bool operator<=(const Integer& other) {
+			_count ++;
 			return _value <= other._value;
 		}
 	private:
