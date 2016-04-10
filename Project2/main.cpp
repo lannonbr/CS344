da8b55285a52774502fb7feebc5a09ac152e6b1c
Benjamin Lannon <lannonbr@clarkson.edu>
2016-04-09 20:53:34 -0400

    Update main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char ** argv) {
   Map <int, string> newMap;
   newMap.insert(3, "Lambda :D");
   newMap.insert(2, "Foobar");
-  newMap.insert()
   cout << newMap.min()->getValue() << '\n';
   cout << newMap.max()->getValue() << '\n';
   return 0;
