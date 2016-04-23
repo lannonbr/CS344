6b35ef37204018beabf4897bf851e7064ccd9866
Hunter Quant <hunterdquant@gmail.com>
2016-04-22 21:09:41 -0400

    added tests

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -10,10 +10,12 @@ int main() {
 
   // Testing empty function
   cout << "Empty: " << newMap.empty() << '\n';
-
+  cout << "Inserting (3, \"Lambda :D\")" << '\n';
   // Inserting a few items
   newMap.insert(3, "Lambda :D");
+  cout << "Inserting (2, \"Foobar\")" << '\n';
   newMap.insert(2, "Foobar");
+  cout << "Inserting (1, \"Cats\")" << '\n';
   newMap.insert(1, "Cats");
 
   //// Testing begin and end functions
@@ -21,26 +23,30 @@ int main() {
   //cout << newMap.end(newMap.top())->getValue() << '\n';
 
   // Testing swap function
+  cout << "Swapping 3 and 1." << '\n';
   newMap.swap(3, 1);
 
   // Testing [] operator
+  cout << "Value at key 3: ";
   cout << newMap[3] << '\n';
+  cout << "Value at key 1: ";
+  cout << newMap[1] << '\n';
   // cout << newMap[4] << '\n'; // Note: This will safely close the program for an invalid index
 
   // Erasing a node
+  cout << "Erasing 2 from the map." << "\n\n";
   newMap.erase(2);
 
-  cout << newMap[1] << '\n';
-
   // cout << newMap.find(newMap.top(), 1)->next() << '\n';
-
+  cout << "Copying map" << '\n';
   Map<int, string> map2(newMap);
-  cout << map2[1] << '\n';
+  cout << "Map copied" << "\n\n";
 
+  cout << "Inserting (40, \"Wat\") into the copied map." << "\n\n";
   map2.insert(40, "Wat");
 
-  cout << "Size of map1: " << newMap.size() << '\n';
-  cout << "Size of map2: " << map2.size() << '\n';
+  cout << "Size of map1: " << newMap.size() << "\n";
+  cout << "Size of map2: " << map2.size() << "\n\n";
 
   newMap.insert(402, "cats 4");
   newMap.insert(3002, "i am a cow");
@@ -48,7 +54,7 @@ int main() {
   for (auto itr = newMap.begin(); itr != newMap.end(); ++itr) {
     cout << itr->second << '\n';
   }
-  cout << "iterator end" << '\n';
+  cout << "iterator end" << "\n\n";
 
   cout << "Testing count on key 2: " << newMap.count(2) << '\n';
   cout << "Testing count on key 3002: " << newMap.count(3002) << '\n';
